f7eb34d3b85bb62da49be4e2b409e505da93eb27
Hyperion1040 <mateusz.worek@gmail.com>
2021-08-13 19:39:05 +0200

    part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,25 +1,49 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle='My Blog Posts'>
-      <p>My cool posts will go in here</p>
+      {/* <p>My cool posts will go in here</p>
       <ul>
         {data.allFile.nodes.map((node) => {
           return <li key={node.name}>{node.name}</li>
         })}
-      </ul>
+      </ul> */}
+      {data.allMdx.nodes.map((node) => {
+        return (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <MDXRenderer>{node.body}</MDXRenderer>
+          </article>
+        )
+      })}
     </Layout>
   )
 }
 
+// export const query = graphql`
+//   query {
+//     allFile {
+//       nodes {
+//         name
+//       }
+//     }
+//   }
+// `
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
